16b23336b9ac0157c437a0d00524b52141313df3
Annie20080920 <juyeong.cheun444@education.nsw.gov.au>
2025-04-01 19:31:37 +1100

    message

--- 0325/hw14.py
+++ 0325/hw14.py
@@ -1,20 +1,14 @@
-students = []
-student_score = []
+name = input("이름을 입력하세요.").split(",")
+score = int(input(f"{name}의 점수를 입력하세요.")).split(",")
 
-for i in range(3):
- name = input("이름을 입력하세요.")
- score = int(input(f"{name}의 점수를 입력하세요."))
- students.append(name)
- student_score.append(float(score))
-
- average = (student_score[0] + student_score[1] + student_score[2]) / len(student_score)
+average = (score[0] + score[1] + score[2]) / len(score)
  
-def print_results(average, students):
- for name, score in students:
+def print_results(average, name, score):
+ for i in zip(name, score):
   if score >= average:
    result = "합격"
   else:
    result = "불합격"
    print(f"{name}: {score}점 -> {result}")
 
-print_results(average, students)
+print_results(average, name, score)
