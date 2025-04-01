96813611b64cc8b389423fc7016491a6ced57326
Annie20080920 <juyeong.cheun444@education.nsw.gov.au>
2025-04-01 18:04:35 +1100

    message

--- 0325/hw3.py
+++ 0325/hw3.py
@@ -1,16 +1,16 @@
 def calculator(operation):
   match operation:
     case ("add", a, b):
-      print(a + b)
+     return(a + b)
     case ("subtract", a, b):
-      print(a - b)
+      return(a - b)
     case ("multiply", a, b):
-      print(a * b)
+      return(a * b)
     case ("divide", a, b):
-      print( a / b)
-    case ("divide", a, b):
-      print("0으로 나눌 수 없습니다.")
+      return( a / b)
+    case ("divide", a, 0):
+      return("0으로 나눌 수 없습니다.")
     case _:
-      print("잘못된 입력")
+      return("잘못된 입력")
 
-calculator(["divide", 10, 2])
+print(calculator(("divide", 10, 0)))
